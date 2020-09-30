7af9ed0ed122138fc19981e53d08bd9e96205226
Blair-White <blairjwhite@hotmail.com>
2020-09-30 14:47:37 -0400

    didn't update this for a while... SO fuckin close.

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -6,20 +6,32 @@ using System.Text;
 using System.Net.Sockets;
 using System.Net;
 using UnityEngine.UIElements;
+using System.Linq;
 
 public class NetworkMan : MonoBehaviour
 {
     public UdpClient udp;
     private string IncomingMessage;
     public string myID;
-    
+    public GameObject PlayerPrefab;
+    ///Too confusing must use long names. 
+    public List<string> PlayersToSpawnList;
+    public List<PlayerCube> PlayersInGameList;
+    public float mX, mY, mZ;
+
+    private void Awake()
+    {
+        myID = "newID";
+        mX = 1; mY = 1; mZ = 1;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        myID = "newID";
+      
+
         udp = new UdpClient();
         
-        udp.Connect("localhost", 12345);
+        udp.Connect("18.219.192.76", 12345);
 
         Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
       
@@ -28,6 +40,9 @@ public class NetworkMan : MonoBehaviour
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
 
         InvokeRepeating("HeartBeat", 1, 1);
+
+        InvokeRepeating("SendStuff", 1, .02f);
+
     }
 
     void OnDestroy(){
@@ -38,7 +53,9 @@ public class NetworkMan : MonoBehaviour
     public enum commands{
         NEW_CLIENT,
         UPDATE,
-        CLIENT_REMOVED
+        CLIENT_REMOVED,
+        GET_PLAYERS_IN_GAME,
+       
     };
     
     [Serializable]
@@ -46,31 +63,34 @@ public class NetworkMan : MonoBehaviour
         public commands cmd;
     
     }
-    
+
+    [Serializable]//This Class is used to send THIS clients cube position to the server, to make a list of all positions.
+    public class PositionMessager {public Vector3 position;}
+        
     [Serializable]
     public class Player{
         [Serializable]
-        public struct receivedColor{
-            public float R;
-            public float G;
-            public float B;
-        }
+        public struct receivedColor{ public float R, G, B; }
+        [Serializable]
+        public struct receivedPosition { public float x, y, z; }
         public string id;
-        public receivedColor color;        
+        public receivedColor color;
+        public receivedPosition position;
     }
 
-    [Serializable]
-    public class NewPlayer{
-        
-    }
+    [Serializable]// We need a class to store incoming new players from server
+    public class NewPlayer { public Player player;}
 
-    [Serializable]
-    public class GameState{
-        public Player[] players;
-    }
+    [Serializable]// We also need a list of dropped players so we can destroy them.
+    public class DroppedPlayer { public string id; public Player[] players;}
 
-    public Message latestMessage;
-    public GameState latestGameState;
+    [Serializable]//Lastly we need a list of the players already in the game for new arrivals to spawn.
+    public class PlayersAlreadyInGame { public Player[] players;}
+
+    [Serializable]//GameState['players'].append(player) this is the players dictionary on the server.
+    public class GameState {public Player[] players;}
+
+    public Message latestMessage; public GameState latestGameState;
     
     void OnReceived(IAsyncResult result){
         // this is what had been passed into BeginReceive as the second parameter:
@@ -92,15 +112,30 @@ public class NetworkMan : MonoBehaviour
             switch(latestMessage.cmd){
                 case commands.NEW_CLIENT:
                     NewPlayerArrived();
+                    NewPlayer p = JsonUtility.FromJson<NewPlayer>(returnData);
+                    PlayersToSpawnList.Add(p.player.id);
+                    if (myID == "newID") { myID = p.player.id; }
                     break;
-                case commands.UPDATE:
+                case commands.UPDATE://When we receive this command from server, update all our cubes. 
                     latestGameState = JsonUtility.FromJson<GameState>(returnData);
-                    if(myID == "newID")
-                    { myID = latestGameState.players[latestGameState.players.Length-1].id; }
+                    
                     break;
-                case commands.CLIENT_REMOVED:
+                case commands.CLIENT_REMOVED:// Someone dropped from the server. 
+                    DroppedPlayer d = JsonUtility.FromJson<DroppedPlayer>(returnData);//Store that player. 
                     Debug.Log("Player Left The Game:");
                     PlayerRemoved();
+                    DestroyPlayers(d.id);//Destroy that player by its id.;
+                    break;
+                case commands.GET_PLAYERS_IN_GAME:
+                    //When a new player connects, get/make an object(list) of all players already in the game.
+                    PlayersAlreadyInGame pigl = JsonUtility.FromJson<PlayersAlreadyInGame>(returnData);
+                    //we need to loop through this list and add them to the spawn list.
+                    for (int i = 0; i < pigl.players.Length; i++)
+                    {
+                        if(pigl.players[i].id != myID)
+                        PlayersToSpawnList.Add(pigl.players[i].id);
+                    }
+                    
                     break;
                 default:
                     Debug.Log("Error");
@@ -115,11 +150,48 @@ public class NetworkMan : MonoBehaviour
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayer(){
-
+    
+    void SpawnPlayersFromList()//Take the list we got from the server to spawn cubes in next function.
+    {
+        if(PlayersToSpawnList.Count > 0)
+        {
+            for(int i = 0; i < PlayersToSpawnList.Count; i++)//loop through our spawn list
+            {
+                InstantiateCubes(PlayersToSpawnList[i]);
+            }
+            PlayersToSpawnList.Clear();//Clear the list when were done.
+            PlayersToSpawnList.TrimExcess();
+        }
     }
 
-    void UpdatePlayers(){
+    void InstantiateCubes(string id)//Spawn Player.
+    {
+
+        
+        GameObject o = Instantiate(PlayerPrefab, Vector3.zero, Quaternion.identity);
+        o.GetComponent<PlayerCube>().ClientID = id;//add the passed id to its id
+        PlayersInGameList.Add(o.GetComponent<PlayerCube>()); //add the cube to our list
+        
+    }
+
+    void UpdatePlayers()
+    {
+        //Loop through all the players and update their position from our gamestate.
+        if (PlayersInGameList.Count < 1) return;
+        if (latestGameState.players.Length < 1) return;
+        for(int i = 0; i < PlayersInGameList.Count; i++)
+        {
+           if(latestGameState.players[i].id != myID)
+            {
+            float XX = latestGameState.players[i].position.x;
+            float YY = latestGameState.players[i].position.y;
+            float ZZ = latestGameState.players[i].position.z;
+            PlayersInGameList[i].transform.position = new Vector3(XX, YY, ZZ);
+
+            }
+            
+             
+        }
      
     }
 
@@ -134,8 +206,14 @@ public class NetworkMan : MonoBehaviour
         Debug.Log(IncomingMessage);
     }
 
-    void DestroyPlayers(){
-
+    void DestroyPlayers(string id){
+        for(int i = 0; i < PlayersInGameList.Count; i++)
+        {
+            if(PlayersInGameList[i].ClientID == id)
+            {
+                Destroy(PlayersInGameList[i].gameObject);
+            }
+        }
     }
     
     void HeartBeat(){
@@ -143,9 +221,19 @@ public class NetworkMan : MonoBehaviour
         udp.Send(sendBytes, sendBytes.Length);
     }
 
+    void SendStuff()
+    {
+        PositionMessager m = new PositionMessager();
+        m.position.x = mX;
+        m.position.y = mY;
+        m.position.z = mZ;
+        Byte[] sendM = Encoding.ASCII.GetBytes(JsonUtility.ToJson(m));
+        udp.Send(sendM, sendM.Length);
+
+    }
+
     void Update(){
-        SpawnPlayer();
+        SpawnPlayersFromList();
         UpdatePlayers();
-        DestroyPlayers();
     }
 }
---
+++ Assets/PlayerCube.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCube : MonoBehaviour
+{
+    public string ClientID;
+    private GameObject nw;
+    public float xx, yy, zz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nw = GameObject.Find("NetworkMan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xx = this.transform.position.x;
+        yy = this.transform.position.y;
+        zz = this.transform.position.z;
+        if(nw.GetComponent<NetworkMan>().myID == this.ClientID)
+        {
+
+        nw.GetComponent<NetworkMan>().mX = xx;
+        nw.GetComponent<NetworkMan>().mY = yy;
+        nw.GetComponent<NetworkMan>().mZ = zz;
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.forward / 36);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(-Vector3.forward / 36);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector3.left / 36);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(-Vector3.left / 36);
+            }
+
+
+
+
+
+
+        }
+        
+
+        
+    }
+}
+
